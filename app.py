20639e3df4ac9347d26d99ac25907df6b663f19a
luis-blash <blash3g@gmail.com>
2021-07-28 18:26:31 -0500

    no recuerdo

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 from flask_migrate import Migrate 
 
 # Modelo de user
-from models.User import db
+from flask_sqlalchemy import SQLAlchemy
 # la ruta de blue print
 from routes.user_bp import user_bp
 
+db = SQLAlchemy()
 # app y configuración
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,7 +15,8 @@ app.config.from_object('config')
 # Iniciamos app
 db.init_app(app)
 # Migramos datos
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
+db.create_all(app=app)
 
 # Registramos la ruta
 app.register_blueprint(user_bp, url_prefix='/usuarios')
